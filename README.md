c75d2754464267b6969b06d84496d7b9bae1d67d
milan44i <mln6stankovic@gmail.com>
2025-05-15 18:38:32 +0200

    setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Supabase & Vue 3 (with Quasar & Pinia)
+
+This is the source code from the **Supabase & Vue 3 (with Quasar & Pinia)** course. Follow this guide to get the project running.
+
+### Install the dependencies
+
+```bash
+npm install
+```
+
+### Start the app in development mode
+
+```bash
+quasar dev
+```
---
+++ src/stores/storeSettings.js
@@ -0,0 +1,45 @@
+import { defineStore } from "pinia";
+import { reactive, watch } from "vue";
+import { Dark, LocalStorage } from "quasar";
+
+export const useStoreSettings = defineStore("settings", () => {
+  const settings = reactive({
+    promptToDelete: true,
+    showRunningBalance: false,
+    currencySymbol: "$",
+    darkMode: false, // false | true | 'auto'
+  });
+
+  // watch darkMode
+  watch(
+    () => settings.darkMode,
+    (value) => {
+      Dark.set(value);
+    },
+    { immediate: true }
+  );
+
+  // watch settings
+  watch(settings, () => {
+    saveSettings();
+  });
+
+  const saveSettings = () => {
+    LocalStorage.set("settings", settings);
+  };
+
+  const loadSettings = () => {
+    const savedSettings = LocalStorage.getItem("settings");
+    if (savedSettings) Object.assign(settings, savedSettings);
+  };
+
+  return {
+    // state
+    settings,
+
+    // getters
+
+    // actions
+    loadSettings,
+  };
+});
